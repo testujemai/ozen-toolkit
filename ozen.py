c94a7716a7ecaeb834cd3bf655c68b6f1baf7904
testujemai <164744062+testujemai@users.noreply.github.com>
2024-07-05 17:23:20 +0200

    Update ozen.py

--- ozen.py
+++ ozen.py
@@ -1,114 +1,221 @@
 import argparse
 import configparser
 import os
+from modules.utils import *
 from datetime import datetime
 import sys
 import colorama
 from tqdm import tqdm
-colorama.init(strip=not sys.stdout.isatty())
-from modules.utils import *
-from termcolor import cprint
+colorama.init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
+from termcolor import cprint 
 from pyfiglet import figlet_format
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert an audio file to WAV format')
     parser.add_argument('file_path', help='Path to the file or directory to convert')
-    parser.add_argument('-output_path', help='Path to the output directory', default=os.getcwd() + os.sep + 'output')
+    parser.add_argument('-output_path', help='Path to the output directory', default=os.getcwd()+os.sep+'output')
     parser.add_argument('-project_name', help='project_name', type=str, default='none')
     parser.add_argument('-whisper_model', help='Which whisper model to use, HF repo address', default='openai/whisper-large-v3')
     parser.add_argument('-device', help='Which device to use, cpu or cuda', default='cuda')
-    parser.add_argument('-mode', help='Automatic diarization and segmentation', default='segment and transcribe', type=str, choices=['auto', 'segment and transcribe', 'diarize', 'transcribe'])
-    parser.add_argument('-diaization_model', help='Which diarization model to use, HF repo address', default='pyannote/speaker-diarization')
-    parser.add_argument('-segmentation_model', help='Which segmentation model to use, HF repo address', default='pyannote/segmentation')
-    parser.add_argument('-seg_onset', help='onset activation threshold, influences the segment detection', default=0.6, type=float)
-    parser.add_argument('-seg_offset', help='offset activation threshold, influences the segment detection', default=0.9, type=float)
-    parser.add_argument('-seg_min_duration', help='minimum duration of a segment, remove speech regions shorter than that many seconds.', default=2.0, type=float)
-    parser.add_argument('-seg_min_duration_off', help='fill non-speech regions shorter than that many seconds.', default=0.0, type=float)
-    parser.add_argument('-hf_token', help='Huggingface token', default='')
+	@@ -27,210 +27,87 @@
     parser.add_argument('-valid_ratio', help='Ratio of validation data', default=0.2, type=float)
     parser.add_argument('-ignore-cofnig', help='Ignore the config, specifiy your own setting sin CLI', action='store_true')
     args = parser.parse_args()
-
-    project_timestamp = generate_timestamp()
-
-    if not args.ignore_cofnig:
+    #check for config.ini
+    if os.path.isfile('config.ini') and not args.ignore_cofnig:
         config = configparser.ConfigParser()
         config.read('config.ini')
-        cfg = config['default']
-        args.project_name = cfg.get('project_name', args.project_name)
-        args.device = cfg.get('device', args.device)
-        args.mode = cfg.get('mode', args.mode)
-        args.diaization_model = cfg.get('diaization_model', args.diaization_model)
-        args.segmentation_model = cfg.get('segmentation_model', args.segmentation_model)
-        args.hf_token = cfg.get('hf_token', args.hf_token)
-        args.valid_ratio = cfg.get('valid_ratio', args.valid_ratio)
-        args.whisper_model = cfg.get('whisper_model', args.whisper_model)
-
-    # Create the output structure
-    output_path = create_output_structure(args.output_path, args.project_name, project_timestamp)
-
-    cprint(figlet_format('PROCESIZNG', font='starwars'), 'yellow', 'on_blue', attrs=['bold'])
-
-    # Convert to WAV if necessary
+        args.whisper_model = config['DEFAULT']['whisper_model']
+        args.device = config['DEFAULT']['device']
+        args.diaization_model = config['DEFAULT']['diaization_model']
+        args.segmentation_model = config['DEFAULT']['segmentation_model']
+        args.valid_ratio = float(config['DEFAULT']['valid_ratio'])
+        args.hf_token = config['DEFAULT']['hf_token']
+        args.seg_onset = float(config['DEFAULT']['seg_onset'])
+        args.seg_offset = float(config['DEFAULT']['seg_offset'])
+        args.seg_min_duration = float(config['DEFAULT']['seg_min_duration'])
+        args.seg_min_duration_off = float(config['DEFAULT']['seg_min_duration_off'])
+    if not os.path.isfile('config.ini') and not args.ignore_cofnig:
+        #create config.ini
+        config = configparser.ConfigParser()
+        config['DEFAULT'] = {'hf_token': ''}
+        config['DEFAULT']['hf_token'] = ''
+        config['DEFAULT']['whisper_model'] = 'openai/whisper-large-v3'
+        config['DEFAULT']['device'] = 'cuda'
+        config['DEFAULT']['diaization_model'] = 'pyannote/speaker-diarization'
+        config['DEFAULT']['segmentation_model'] = 'pyannote/segmentation'
+        config['DEFAULT']['valid_ratio'] = str(0.2)
+        config['DEFAULT']['seg_onset'] = str(0.6)
+        config['DEFAULT']['seg_offset'] = str(0.9)
+        config['DEFAULT']['seg_min_duration'] = str(2.0)
+        config['DEFAULT']['seg_min_duration_off'] = str(0.0)
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+    #check if thre's a .TOKEN file in the current directory
+    cprint(figlet_format('OZEN', font='starwars'),
+       'yellow', 'on_black', attrs=['bold'])
+    #cprint(figlet_format('TOOLKIT', font='starwars'),
+    #   'yellow', 'on_black', attrs=['bold'])
+    if args.hf_token == '':
+            #colorama.init()
+            print(colorama.Fore.RED + 'No Huggingface Token found' + colorama.Fore.RESET)
+            #prompt for token
+            args.hf_token = input('This tool uses PyAnnote which requires the user to accept its terms on the model page\nPlease visit https://huggingface.co/pyannote/segmentation and create a token to paste here. \nTo continue enter your Huggingface token: ')
+            if args.hf_token == '':
+                print('No token entered, exiting...')
+                exit()
+            elif args.hf_token != '' and not args.ignore_cofnig:
+                config['DEFAULT']['hf_token'] = args.hf_token
+                with open('config.ini', 'w') as configfile:
+                    config.write(configfile)
+    if not args.file_path:
+        parser.print_help()
+        exit()
+    file_path = args.file_path
     original_file_path = args.file_path
-    if not original_file_path.endswith('.wav'):
-        args.file_path = convert_to_wav(args.file_path)
+    base_name = os.path.splitext(os.path.basename(original_file_path))[0]
+    timestamp = generate_timestamp()
+    if args.project_name == 'none':
+        args.project_name = os.path.basename(os.path.normpath(file_path))
+    output_path = create_output_structure(args.output_path, args.project_name, timestamp)
+    train_file = os.path.join(output_path, 'train.txt')
+    valid_file = os.path.join(output_path, 'valid.txt')
+    wavs_path = os.path.join(output_path, 'wavs')
+    if os.path.isfile(file_path):
+        # Convert a single file
+        file_ext = os.path.splitext(file_path)[-1].lower()
+        file_path, filename = os.path.split(file_path)
+        if file_ext in ['.mp3', '.m4a', '.flac', '.ogg', '.wav']:
+            if file_ext != '.wav':
+                print(colorama.Fore.GREEN + 'Converting to WAV...' + colorama.Fore.RESET)
+                file_path = convert_to_wav(os.path.join(file_path, filename))
+            else:
+                    file_path = os.path.join(file_path, filename)
+            if args.mode == 'auto':
+                print(colorama.Fore.GREEN + 'Loading Diraization Model...' + colorama.Fore.RESET)
+                pipe = load_pyannote_audio_pipeline(args.diaization_model, args.hf_token)
+                print(colorama.Fore.GREEN + 'Diarizing...' + colorama.Fore.RESET)
+                diarization = diarize_audio_file(file_path, pipe)
+                #mil = millisec(diarization)
+                print(colorama.Fore.GREEN + 'Grouping Diarization...' + colorama.Fore.RESET)
+                dir_groups = group_diarization(diarization)
+                groups = group_diarization(diarization)
+                print(colorama.Fore.GREEN + 'Segmenting...' + colorama.Fore.RESET)
+                wavs = segment_file_by_diargroup(file_path, groups)
+                del pipe
+                print(colorama.Fore.GREEN + 'Loading Transcribing Model...' + colorama.Fore.RESET)
+                transcribe_pipe = init_transcribe_pipeline(args.whisper_model, args.device)
+                #create progress bar
+                print(colorama.Fore.GREEN + 'Transcribing...' + colorama.Fore.RESET)
+                pb = tqdm(total=len(groups))
+                for wav in range(0,wavs):
+                    wav = str(wav)
+                    res = transcribe_audio(os.path.join(os.getcwd(), wav+'.wav'),transcribe_pipe)
+                    pb.update(1)
+            if args.mode == 'segment and transcribe':
+                print(colorama.Fore.GREEN + 'Loading Segment Model...' + colorama.Fore.RESET)
+                pipe = load_pyannote_audio_model(args.segmentation_model, args.hf_token)
+                segments = segment_audio_file(file_path, pipe, args.seg_onset, args.seg_offset, args.seg_min_duration, args.seg_min_duration_off)
+                #milisecs = millisec(segments)
+                print(colorama.Fore.GREEN + 'Segmenting...' + colorama.Fore.RESET)
+                groups = group_segmentation(segments)
+                wavs = segment_file_by_diargroup(file_path,wavs_path, groups)
+                del pipe
+
+                print(colorama.Fore.GREEN + 'Loading Transcribing Model...' + colorama.Fore.RESET)
+
+                transcribe_pipe = init_transcribe_pipeline(args.whisper_model, 0 if args.device == 'cuda' else -1)
+                print(colorama.Fore.GREEN + 'Transcribing...' + colorama.Fore.RESET)
+                amount_to_train = int(len(groups) * (1 - args.valid_ratio))
+                amount_to_valid = len(groups) - amount_to_train
+                pb = tqdm(total=len(groups))
+                for wav in range(0,amount_to_train):
+                    wav = str(wav)
+                    res = transcribe_audio(os.path.join(wavs_path, base_name+'-'+wav+'.wav'),transcribe_pipe)[1:]
+                    #add to train file
+                    add_to_textfile(train_file, 'wavs/'+base_name+'-'+wav+'.wav|'+res+'\n')
+                    pb.update(1)
+                for wav in range(amount_to_train,amount_to_train+amount_to_valid):
+                    wav = str(wav)
+                    res = transcribe_audio(os.path.join(wavs_path, base_name+'-'+wav+'.wav'),transcribe_pipe)[1:]
+                    #add to valid file
+                    add_to_textfile(valid_file, 'wavs/'+base_name+'-'+wav+'.wav|'+res+'\n')
+                    pb.update(1)
+    elif os.path.isdir(file_path):
+        # Convert all files in the directory
+        gidx = -1
+        for filename in os.listdir(file_path):
+            file_path = args.file_path
+            file_ext = os.path.splitext(filename)[1].lower()
+            if file_ext in ['.mp3', '.m4a', '.flac', '.ogg', '.wav']:
+                if file_ext != '.wav':
+                    print(colorama.Fore.GREEN + 'Converting to WAV...' + colorama.Fore.RESET)
+                    file_path = convert_to_wav(os.path.join(file_path, filename))
+                else:
+                    file_path = os.path.join(file_path, filename)
+                if args.mode == 'auto':
+                    print(colorama.Fore.GREEN + 'Loading Diraization Model...' + colorama.Fore.RESET)
+                    pipe = load_pyannote_audio_model(args.diaization_model, args.hf_token)
+                    print(colorama.Fore.GREEN + 'Diarizing...' + colorama.Fore.RESET)
+                    diarization = diarize_audio_file(file_path, pipe)
+                    mil = millisec(diarization)
+                    print(colorama.Fore.GREEN + 'Grouping Diarization...' + colorama.Fore.RESET)
+                    dir_groups = group_diarization(diarization)
+                    groups = group_diarization(diarization)
+                    print(colorama.Fore.GREEN + 'Segmenting...' + colorama.Fore.RESET)
+                    wavs = segment_file_by_diargroup(file_path, groups, gidx)
+                    del pipe
+                    print(colorama.Fore.GREEN + 'Loading Transcribing Model...' + colorama.Fore.RESET)
+                    transcribe_pipe = init_transcribe_pipeline(args.whisper_model, 0 if args.device == 'cuda' else -1)
+                    print(colorama.Fore.GREEN + 'Transcribing...' + colorama.Fore.RESET)
+                    pb = tqdm(total=len(groups))
+                    for wav in range(0,amount_to_train):
+                        gidx += 1
+                        wav = str(wav)
+                        res = transcribe_audio(os.path.join(wavs_path, wav+'.wav'),transcribe_pipe)[1:]
+                        #add to train file
+                        add_to_textfile(train_file, 'wavs/'+wav+'.wav|'+res+'\n')
+                        pb.update(1)
+                    for wav in range(amount_to_train,amount_to_train+amount_to_valid):
+                        gidx += 1
+                        wav = str(wav)
+                        res = transcribe_audio(os.path.join(wavs_path, wav+'.wav'),transcribe_pipe)[1:]
+                        #add to valid file
+                        add_to_textfile(valid_file, 'wavs/'+wav+'.wav|'+res+'\n')
+                        pb.update(1)
+                if args.mode == 'segment and transcribe':
+                    print(colorama.Fore.GREEN + 'Loading Segment Model...' + colorama.Fore.RESET)
+                    pipe = load_pyannote_audio_model(args.segmentation_model, args.hf_token)
+                    segments = segment_audio_file(file_path, pipe, args.seg_onset, args.seg_offset, args.seg_min_duration, args.seg_min_duration_off)
+                    #milisecs = millisec(segments)
+                    print(colorama.Fore.GREEN + 'Segmenting...' + colorama.Fore.RESET)
+                    groups = group_segmentation(segments)
+                    wavs = segment_file_by_diargroup(file_path,wavs_path, groups, gidx)
+                    del pipe
+
+                    transcribe_pipe = init_transcribe_pipeline(args.whisper_model, 0 if args.device == 'cuda' else -1)
+                    print(colorama.Fore.GREEN + 'Loading Transcribing Model...' + colorama.Fore.RESET)
+                    amount_to_train = int(len(groups) * (1 - args.valid_ratio))
+                    amount_to_valid = len(groups) - amount_to_train
+                    print(colorama.Fore.GREEN + 'Transcribing...' + colorama.Fore.RESET)
+                    pb = tqdm(total=len(groups))
+
+                    for wav in range(0,amount_to_train):
+                        gidx += 1
+                        wav = str(wav)
+                        res = transcribe_audio(os.path.join(wavs_path, base_name+'-'+str(gidx)+'.wav'),transcribe_pipe)[1:]
+                        #add to train file
+                        add_to_textfile(train_file, 'wavs/'+base_name+'-'+str(gidx)+'.wav|'+res+'\n')
+                        pb.update(1)
+                    for wav in range(amount_to_train,amount_to_train+amount_to_valid):
+                        gidx += 1
+                        wav = str(wav)
+                        res = transcribe_audio(os.path.join(wavs_path, base_name+'-'+str(gidx)+'.wav'),transcribe_pipe)[1:]
+                        #add to valid file
+                        add_to_textfile(valid_file, 'wavs/'+base_name+'-'+str(gidx)+'.wav|'+res+'\n')
+                        pb.update(1)
+
+
     else:
-        audio = AudioSegment.from_file(args.file_path)
-        spacer_milli = 2000
-        spacer = AudioSegment.silent(duration=spacer_milli)
-        audio = spacer.append(audio, crossfade=0)
-        audio.export(args.file_path, format='wav')
-
-    diarization_pipeline = None
-    segmentation_model = None
-    if args.mode == 'auto':
-        diarization_pipeline = load_pyannote_audio_pipeline(args.diaization_model, args.hf_token)
-        segmentation_model = load_pyannote_audio_model(args.segmentation_model, args.hf_token)
-    elif args.mode == 'segment and transcribe':
-        segmentation_model = load_pyannote_audio_model(args.segmentation_model, args.hf_token)
-    elif args.mode == 'diarize':
-        diarization_pipeline = load_pyannote_audio_pipeline(args.diaization_model, args.hf_token)
-
-    if args.mode == 'diarize' or args.mode == 'auto':
-        diarization = diarize_audio_file(args.file_path, diarization_pipeline)
-        diarization_groups = group_diarization(diarization)
-        gidx = segment_file_by_diargroup(args.file_path, os.path.join(output_path, 'wavs'), diarization_groups, original_file_path)
-        with open(os.path.join(output_path, 'train.txt'), 'w') as f:
-            f.write('')
-        with open(os.path.join(output_path, 'valid.txt'), 'w') as f:
-            f.write('')
-        base_name = os.path.splitext(os.path.basename(original_file_path))[0]
-        for idx, g in enumerate(diarization_groups):
-            if idx < len(diarization_groups) * (1 - args.valid_ratio):
-                with open(os.path.join(output_path, 'train.txt'), 'a') as f:
-                    f.write(f'wavs/{base_name}-{idx}.wav\n')
-            else:
-                with open(os.path.join(output_path, 'valid.txt'), 'a') as f:
-                    f.write(f'wavs/{base_name}-{idx}.wav\n')
-
-    if args.mode == 'segment and transcribe' or args.mode == 'auto':
-        segmentation = segment_audio_file(args.file_path, segmentation_model, args.seg_onset, args.seg_offset, args.seg_min_duration, args.seg_min_duration_off)
-        segmentation_groups = group_segmentation(segmentation)
-        gidx = segment_file_by_diargroup(args.file_path, os.path.join(output_path, 'wavs'), segmentation_groups, original_file_path)
-        with open(os.path.join(output_path, 'train.txt'), 'w') as f:
-            f.write('')
-        with open(os.path.join(output_path, 'valid.txt'), 'w') as f:
-            f.write('')
-        base_name = os.path.splitext(os.path.basename(original_file_path))[0]
-        for idx, g in enumerate(segmentation_groups):
-            if idx < len(segmentation_groups) * (1 - args.valid_ratio):
-                with open(os.path.join(output_path, 'train.txt'), 'a') as f:
-                    f.write(f'wavs/{base_name}-{idx}.wav\n')
-            else:
-                with open(os.path.join(output_path, 'valid.txt'), 'a') as f:
-                    f.write(f'wavs/{base_name}-{idx}.wav\n')
-
-    if args.mode == 'transcribe' or args.mode == 'auto':
-        pipe = init_transcribe_pipeline(args.whisper_model, 0 if args.device == 'cuda' else -1)
-        for idx in range(gidx + 1):
-            file_path = os.path.join(output_path, 'wavs', f'{os.path.splitext(os.path.basename(original_file_path))[0]}-{idx}.wav')
-            transcription = transcribe_audio(file_path, pipe)
-            add_to_textfile(os.path.join(output_path, 'train.txt'), f'{os.path.basename(file_path)}|{transcription}\n')
-
-    cprint(figlet_format('DONE', font='starwars'), 'green', 'on_blue', attrs=['bold'])
+        print(f'Error: {file_path} is not a file or directory.')
+    print(colorama.Fore.GREEN + ' Done!' + colorama.Fore.RESET)
+    print('')
